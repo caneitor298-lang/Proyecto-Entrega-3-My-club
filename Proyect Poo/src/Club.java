d588e467ddc22a6a68ca8066c625619fd5170f18
Jorge Emilio Cano Itzep <caneitor298@gmail.com>
2026-09-24 21:59:46 -0600

    Update Club.java

--- Proyect Poo/src/Club.java
+++ Proyect Poo/src/Club.java
@@ -1,5 +1,7 @@
+import java.io.Serializable;
 import java.util.ArrayList;
-public class Club {
+public class Club implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private ArrayList<Anuncio> anuncios;
 
