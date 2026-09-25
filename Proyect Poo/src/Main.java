b2e0f155d8702ab5380d3dfc620ffa781a5e4ec3
Jorge Emilio Cano Itzep <caneitor298@gmail.com>
2026-09-24 21:55:28 -0600

    Update Main.java

--- Proyect Poo/src/Main.java
+++ Proyect Poo/src/Main.java
@@ -1,13 +1,21 @@
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Los datos iniciales se crean otra vez cada vez que se abre el programa.
-        DatosAplicacion datos = DatosAplicacion.crearDemostracion();
+        DatosAplicacion datos = DatosAplicacion.cargar();
         UsuarioController usuarios = new UsuarioController(datos);
         ClubController clubes = new ClubController(datos);
         SwingUtilities.invokeLater(() -> {
-            new VentanaPrincipal(usuarios, clubes).setVisible(true);
+            VentanaPrincipal ventana = new VentanaPrincipal(usuarios, clubes, datos);
+            ventana.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    datos.guardar();
+                }
+            });
+            ventana.setVisible(true);
         });
     }
 }
